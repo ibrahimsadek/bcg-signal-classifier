c02df4b77bc9fc3bb7e26536f075d3ed391923cf
Ibrahim Sadek <ibrahimsadek87@gmail.com>
2026-02-10 21:18:47 +0200

    Suppress TensorFlow logs in main.py
    Suppress TensorFlow logging by setting environment variable.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 """Entry point for BCG signal classification pipeline."""
 
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Suppress INFO + WARNING logs
+
 from bcg_signal_classifier.pipeline import main
 
 if __name__ == "__main__":
